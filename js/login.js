de2bb24b6281ec90cedd110a63037988647041be
Nathan Phan <nathanphan21@gmail.com>
2023-11-28 19:07:07 -0500

    created signup box and added code to loginjs

--- js/login.js
+++ js/login.js
@@ -22,32 +22,32 @@ const loginFormHandler = async (event) => {
       }
     };
     
-    // const signupFormHandler = async (event) => {
-    //     event.preventDefault();
-    //     //we could have a name section
-    //     // const name = document.querySelector('#name-signup').value.trim();
-    //     const username = document.querySelector('#username').value.trim();
-    //     const password = document.querySelector('#password').value.trim();
+    const signupFormHandler = async (event) => {
+        event.preventDefault();
+        //we could have a name section
+        const name = document.querySelector('#name').value.trim();
+        const username = document.querySelector('#username').value.trim();
+        const password = document.querySelector('#password').value.trim();
       
-    //     if (username && password) {
-    //       const response = await fetch('/api', {
-    //         method: 'POST',
-    //         body: JSON.stringify({ username, password }),
-    //         headers: { 'Content-Type': 'application/json' },
-    //       });
+        if (name && username && password) {
+          const response = await fetch('/api', {
+            method: 'POST',
+            body: JSON.stringify({name, username, password }),
+            headers: { 'Content-Type': 'application/json' },
+          });
       
-    //       if (response.ok) {
-    //         document.location.replace('/main');
-    //       } else {
-    //         alert(response.statusText);
-    //       }
-    //     }
-    //   };
+          if (response.ok) {
+            document.location.replace('/main');
+          } else {
+            alert(response.statusText);
+          }
+        }
+      };
 
   document
   .querySelector('.login-container')
   .addEventListener('submit', loginFormHandler);
 
-// document
-//   .querySelector('.signup-form')
-//   .addEventListener('submit', signupFormHandler);
+document
+  .querySelector('.signup-container')
+  .addEventListener('submit', signupFormHandler);
