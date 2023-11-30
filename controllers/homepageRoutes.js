05e972a9afef9eeb3f34913323216c56b11009da
Vincent Baraga <vincentb.baraga@gmail.com>
2023-11-30 12:14:39 -0500

    fixed the paths for the static files and login.js script source

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { layout: "main" });
 });
 
 module.exports = router;
