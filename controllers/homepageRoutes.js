b6a84e21176634477286de6fda4758e7aea5c278
Vincent Baraga <vincentb.baraga@gmail.com>
2023-11-30 20:28:11 -0500

    added a route for the main homepage and a log out route for users, as well as a new model

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  try {
+    res.render("main");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.get("/login", (req, res) => {
-  res.render("login", { layout: "main" });
+  try {
+    res.render("login", { layout: "main" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
+//a good looking homepage that has a nav bar to log in or view the executive louge benefits
+//the page that shows you the benefits, a handlebars if statement that only shows the memeber benefits if logged_in
+//the get /menu route will findAll from Benefit and save it to a variable, then .map it in a new variable,
+//then res.render the menu page with the array variable as an option
+//then do a for each inside the menu page which then uses a partial for the specifics
--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
-    console.log(`Now listening @ http://localhost:${PORT}/login`)
+    console.log(`Now listening @ http://localhost:${PORT}`)
   );
 });
